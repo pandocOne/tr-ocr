a738e07a28290444d901c5761ffe9d4dca1b9ffb
Ajeep <ajeep8@gmail.com>
2023-07-03 15:31:45 +0800

    add tr-ocr.sh, to call tr-ocr docker

--- tr-ocr.sh
+++ tr-ocr.sh
@@ -3,3 +3,4 @@
 #docker run -it --rm --cpus=8 -v $(pwd):/data -e uid=`id -u` ajeep/tr-ocr:v2.3.1 24
 docker run -it --rm --cpus=8 -v $(pwd):/data -e uid=`id -u` ajeep/tr-ocr:v2.3.1
 # sed -i ':a;N;$!ba;s/\([^\n]\)\n\([^\n]\)/\1\2/g' file
+# sed -e ':a' -e 'N' -e '$!ba' -e 's/\([^\n]\)\n\([^\n]\)/\1\2/g' file
